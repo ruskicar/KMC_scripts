2a3c0c6ca03298425493389a44d1fb0877701353
ruskicar <ruskicar@siol.net>
2018-01-26 11:32:13 +0100

    Update count_reactions.py

--- count_reactions.py
+++ count_reactions.py
@@ -2,7 +2,6 @@
 #import matplotlib.pyplot as plt
 
 reactions=[]
-
 with open('mechanism_input.dat', 'r') as mechanism:
 	for line in mechanism:
 		if line.startswith('step '):
@@ -10,11 +9,10 @@ with open('mechanism_input.dat', 'r') as mechanism:
 		if line.startswith('reversible_step '):
 			reactions.extend([line[16:-1]+"_fwd",0])
 			reactions.extend([line[16:-1]+"_rev",0])
-	with open('general_output.txt', 'r') as file1:
-		for line1 in file1:
-			if line1.startswith('   Elementary step '): reactions[reactions.index(line1[19:-1])+1]+=1
+				
+with open('general_output.txt', 'r') as file1:
+	for line1 in file1:
+		if line1.startswith('   Elementary step '):	reactions[reactions.index(line1[19:-1])+1]+=1
 
-print "#",
-for k in range(0,len(reactions)/2): print reactions[::2][k],
-print '\n',
-for k in range(0,len(reactions)/2): print str(reactions[1::2][k]),
+for i in range(0,len(reactions)/2):
+	print reactions[::2][i] + " : " + str(reactions[1::2][i])
